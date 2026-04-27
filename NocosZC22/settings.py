24e19505a6740d5a22e2796533d2f2940db8f32c
aljoshua-nox <anocos@gbox.adnu.edu.ph>
2026-04-28 01:20:35 +0800

    feat: add custom 404 error page template

--- NocosZC22/settings.py
+++ NocosZC22/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-=#+#mi9y-4*rit7oi@t57nt6_v8pm+tagor!0^avv(hp4&lqo+"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
